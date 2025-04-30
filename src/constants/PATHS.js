dc73c62ee0372059f9464087763fea32b9990925
M-NAWJHA <m.nawjha@gmail.com>
2025-04-30 12:47:09 +0300

    Create App Paths Constants.

---
+++ src/constants/PATHS.js
@@ -0,0 +1,36 @@
+export const PATHS = {
+  // Main App Paths.
+  HOME: '/home',
+  ABOUT: '/about-us',
+  SERVICES: '/our-services',
+  PROJECTS: '/our-projects',
+  REQUEST: '/request',
+  CONTACT: '/contact-us',
+  // Dashboard Paths.
+  DASHBOARD: {
+    HOME: '/dashboard',
+    SERVICES: {
+      VIEW: '/view-services',
+      CREATE: '/create-services'
+    },
+    PROJECTS: {
+      VIEW: '/view-projects',
+      CREATE: '/create-projects'
+    },
+    REVIEWS: {
+      VIEW: '/view-reviews',
+      CREATE: '/create-reviews'
+    },
+    REQUESTS: '/view-requests',
+    MASSAGES: '/view-massages',
+    SETTING: {
+      ABOUT: '/about-settings',
+      PUBLIC: '/public-settings',
+      ADMIN: '/admin-settings'
+    }
+  },
+  // Auth Paths.
+  AUTH:{
+    SIGNIN: '/app-signin'
+  }
+};
