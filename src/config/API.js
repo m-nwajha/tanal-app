6a1f411b9403b3d2b7b8179d901f4c8a5f149626
M-NAWJHA <m.nawjha@gmail.com>
2025-05-01 12:28:47 +0300

    Create JSON API & handel useAPI hook.

---
+++ src/config/API.js
@@ -0,0 +1,5 @@
+// API URL.
+export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+// API Key.
+export const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
---
+++ src/hooks/useAPI.jsx
@@ -0,0 +1,65 @@
+'use client';
+// Import React Hooks
+import { useState } from 'react';
+
+// Import Axios.
+import axios from 'axios';
+
+const useAPI = (url, config) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const get = async () => {
+    try {
+      const res = await axios.get(url, config);
+      setData(res.data);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  const del = async id => {
+    try {
+      const res = await axios.delete(`${url}/${id}`, config);
+      setData(data.filter(item => item.id !== id));
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  const post = async body => {
+    try {
+      const res = await axios.post(`${url}`, body, config);
+      setData(prevState => [...prevState, res.data]);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  const put = async body => {
+    try {
+      const res = await axios.post(`${url}`, body, config);
+      setData(prevState =>
+        prevState.map(item => {
+          return body.id === item.id ? res.data : item;
+        })
+      );
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  return {
+    data,
+    loading,
+    error,
+    get,
+    del,
+    post,
+    put
+  };
+};
+
+export default useAPI;
