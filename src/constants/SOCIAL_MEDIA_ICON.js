8162d396401794890837cd0db9e2f76238031d6d
M-NAWJHA <m.nawjha@gmail.com>
2025-05-02 13:14:16 +0300

    Create SocialMedia in Footer Components.

---
+++ src/constants/SOCIAL_MEDIA_ICON.js
@@ -0,0 +1,20 @@
+import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
+import XIcon from '@mui/icons-material/X';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
+export const SOCIAL_MEDIA_ICON = {
+  'facebook': {
+    icon: <FacebookRoundedIcon fontSize='small' />,
+    URL: 'https://www.facebook.com/'
+  },
+  'twitter-x': { icon: <XIcon fontSize='small' />, URL: 'https://x.com/' },
+  'instagram': {
+    icon: <InstagramIcon fontSize='small' />,
+    URL: 'https://www.instagram.com/'
+  },
+  'linkedin': {
+    icon: <LinkedInIcon fontSize='small' />,
+    URL: 'https://www.linkedin.com/in/'
+  }
+};
