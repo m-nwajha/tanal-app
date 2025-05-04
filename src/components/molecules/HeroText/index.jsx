e94a863919255a0be3ffe8bb8b00e1a496b227d2
M-NAWJHA <m.nawjha@gmail.com>
2025-05-04 10:07:35 +0300

    Handel HeroText component.

---
+++ src/components/molecules/HeroText/index.jsx
@@ -0,0 +1,36 @@
+// Import MUI Components.
+import { Stack, Skeleton } from '@mui/material';
+
+// Import Components.
+import H from '@/components/atoms/HeadingSection';
+import D from '@/components/atoms/DescriptionSection';
+
+const HeroText = ({ getData, loading }) => {
+  return (
+    <Stack
+      spacing={3}>
+      {loading ? (
+        <Skeleton
+          sx={{width: '100%', height: '5rem' }}
+          variant='text'
+        />
+      ) : (
+        <H
+          variant='h1'
+          type='light'>
+          {getData?.hero?.title}
+        </H>
+      )}
+      {loading ? (
+        <Skeleton
+          sx={{ width: '100%', height: '5rem' }}
+          variant='text'
+        />
+      ) : (
+        <D type='light'>{getData?.hero?.description}</D>
+      )}
+    </Stack>
+  );
+};
+
+export default HeroText;
