4cf9842d2cc1ce4c9d15fb59fecf22e781c2493a
M-NAWJHA <m.nawjha@gmail.com>
2025-05-05 15:56:46 +0300

    create AboutText.

---
+++ src/components/molecules/AboutText/index.jsx
@@ -0,0 +1,30 @@
+// Import MUI Components.
+import { Stack, Typography } from '@mui/material';
+
+// Import Hooks.
+import { useTheme } from '@emotion/react';
+
+// Import Components.
+import H from '@/components/atoms/HeadingSection';
+
+const AboutText = ({ title, description }) => {
+  const { colors, fontFamily } = useTheme();
+  return (
+    <Stack spacing={4}>
+      <H
+        type='dark'
+        variant='h4'>
+        {title}
+      </H>
+      <Typography
+        lineHeight={2}
+        variant='body1'
+        fontFamily={fontFamily.IBM}
+        color={colors.quinary}>
+        {description}
+      </Typography>
+    </Stack>
+  );
+};
+
+export default AboutText;
