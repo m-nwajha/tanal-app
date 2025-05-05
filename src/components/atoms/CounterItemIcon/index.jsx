4c2a9813670e1876be3859f497c144abce7e4daf
M-NAWJHA <m.nawjha@gmail.com>
2025-05-05 14:23:32 +0300

    create counter Item Icon .

---
+++ src/components/atoms/CounterItemIcon/index.jsx
@@ -0,0 +1,8 @@
+// Import Style.
+import { StyledCounterItemIcon } from './style';
+
+const CounterItemIcon = ({ icon }) => {
+  return <StyledCounterItemIcon>{icon}</StyledCounterItemIcon>;
+};
+
+export default CounterItemIcon;
