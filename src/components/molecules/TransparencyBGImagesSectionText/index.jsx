31f09a73000af5a7ee58927064da4751f86758c9
M-NAWJHA <m.nawjha@gmail.com>
2025-05-10 16:27:46 +0300

    handel Skeleton.

--- src/components/molecules/TransparencyBGImagesSectionText/index.jsx
+++ src/components/molecules/TransparencyBGImagesSectionText/index.jsx
@@ -1,13 +1,42 @@
 // Import MUI Components.
-import { Typography } from '@mui/material';
+import { Typography, Skeleton, Stack } from '@mui/material';
 
 // Import Style.
 import { StyledText } from './style';
 
-const TransparencyBGImagesSectionText = ({ textData }) => {
+const TransparencyBGImagesSectionText = ({ textData, loading }) => {
   return (
     <StyledText>
-      <Typography variant='body1'>{textData}</Typography>
+      {loading ? (
+        <Stack
+          width='100%'
+          spacing={1}>
+          <Skeleton
+            variant='text'
+            width='100%'
+            height='3rem'
+          />
+          <Skeleton
+            variant='text'
+            width='100%'
+            height='3rem'
+          />
+
+          <Skeleton
+            variant='text'
+            width='100%'
+            height='3rem'
+          />
+
+          <Skeleton
+            variant='text'
+            width='50%'
+            height='3rem'
+          />
+        </Stack>
+      ) : (
+        <Typography variant='body1'>{textData}</Typography>
+      )}
     </StyledText>
   );
 };
