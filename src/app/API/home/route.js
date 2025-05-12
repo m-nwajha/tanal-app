740c65697f18ca94d7b68ce31f05bbce1d3126ef
M-NAWJHA <m.nawjha@gmail.com>
2025-05-12 13:19:23 +0300

    Handel the home route in get.

---
+++ src/app/API/home/route.js
@@ -0,0 +1,19 @@
+// Import Next.js.
+import { NextResponse } from 'next/server';
+
+// Import Config.
+import connectMongodb from '@/config/mongodb';
+
+// Import Models.
+import Home from '@/models/home';
+
+// Import Utils.
+import { withApiKeyProtection } from '@/utils/apiKey';
+
+async function handler(req) {
+    await connectMongodb();
+    const homeData = await Home.findOne();
+    return NextResponse.json(homeData);
+}
+
+export const GET = withApiKeyProtection(handler);
