7ff6f5a88d0102e74a5d3ea8adeb23b7c3476461
M-NAWJHA <m.nawjha@gmail.com>
2025-05-12 13:12:48 +0300

    create COUNTERS Constants.

--- src/constants/HOME.js
+++ src/constants/HOME.js
@@ -1,8 +1,29 @@
+// Import Paths App.
+import { PATHS } from './PATHS';
+
+// Import Icons.
 import PeopleRoundedIcon from '@mui/icons-material/PeopleRounded';
 import BadgeRoundedIcon from '@mui/icons-material/BadgeRounded';
 import AccountTreeRoundedIcon from '@mui/icons-material/AccountTreeRounded';
 import TagFacesRoundedIcon from '@mui/icons-material/TagFacesRounded';
+
+
 export const HOME = {
+  HERO: {
+    title: 'شركة تنال',
+    description: 'الأولى للإستثمار',
+    btns: {
+      more: {
+        label: 'تعرف المزيد',
+        href: PATHS.ABOUT
+      }
+    },
+    images: {
+      img1: '/assets/images/hero-img1.webp',
+      img2: '/assets/images/hero-img2.webp',
+      img3: '/assets/images/hero-img3.webp'
+    }
+  },
   COUNTERS: {
     title: 'تقهوة عند تنال',
     description: 'نجاحات تنال الأولى للاستثمار بالأرقام',
@@ -14,6 +35,6 @@ export const HOME = {
     }
   },
   ABOUT_US: {
-    title: "تنال بين السطور",
+    title: 'تنال بين السطور'
   }
 };
