74c7760b852fcb357415dea308c3ff1b066e1c9e
Mohamed Alnawjha <m.nawjha@gmail.com>
2025-05-17 13:17:43 +0300

    .

--- src/app/(main)/about-us/page.jsx
+++
@@ -1,8 +0,0 @@
-import AboutPage from '@/components/pages/AboutPage';
-import { PAGES_META_DATA } from '@/constants/META_DATA';
-export const metadata = {
-  title: PAGES_META_DATA.about
-};
-const AboutUs = () => <AboutPage />
-
-export default AboutUs
