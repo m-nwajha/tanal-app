2bc09ff4740474070f320c998a081ba36fc85bba
Mohame ALnawjha <m.nawjha@gmail.com>
2025-05-27 12:41:43 +0300

    .

--- src/constants/END_POINTS.js
+++ src/constants/END_POINTS.js
@@ -1,9 +1,12 @@
 import { API_URL } from '@/config/API';
 
 export const END_POINTS = {
-  HOME: `${API_URL}/home`,
-  PROJECTS: `${API_URL}/projects`,
-  FOOTER: `${API_URL}/footer`,
-  ABOUT_US: `${API_URL}/about`,
-  OUR_SERVICES: `${API_URL}/services`,
+  SERVICES: `${API_URL}/services`,
+  PROJECTS: `${API_URL}/project`,
+  REVIEW: `${API_URL}/review`,
+  COUNTER: `${API_URL}/counter`,
+  CONTACT: `${API_URL}/contact`,
+  CONTACT_INFO: `${API_URL}/contact-info`,
+  BOARD_DIRECTORS: `${API_URL}/boardDirectors`,
+  BETWEEN_LINES: `${API_URL}/between-lines`
 };
