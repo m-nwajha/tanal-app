cd803e81b8c32215d92dec4a5ab3ef584042d9d6
Mohame ALnawjha <m.nawjha@gmail.com>
2025-05-27 12:39:57 +0300

    so icons

--- src/constants/SOCIAL_MEDIA_ICON.js
+++ src/constants/SOCIAL_MEDIA_ICON.js
@@ -1,20 +1,23 @@
 import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
-import XIcon from '@mui/icons-material/X';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import XIcon from '@mui/icons-material/X';
 
 export const SOCIAL_MEDIA_ICON = {
   'facebook': {
     icon: <FacebookRoundedIcon fontSize='small' />,
-    URL: 'https://www.facebook.com/'
+    URL: 'https://www.facebook.com/',
   },
-  'twitter-x': { icon: <XIcon fontSize='small' />, URL: 'https://x.com/' },
   'instagram': {
     icon: <InstagramIcon fontSize='small' />,
-    URL: 'https://www.instagram.com/'
+    URL: 'https://www.instagram.com/',
   },
   'linkedin': {
     icon: <LinkedInIcon fontSize='small' />,
-    URL: 'https://www.linkedin.com/in/'
+    URL: 'https://www.linkedin.com/in/',
+  },
+  'twitter-x': {
+    icon: <XIcon fontSize='small' />,
+    URL: 'https://x.com/',
   }
 };
