5af6040d731d3e9090e852ba0adf4a569441a51e
Mohame ALnawjha <m.nawjha@gmail.com>
2025-06-04 15:43:05 +0300

    .

--- next.config.mjs
+++ next.config.mjs
@@ -3,5 +3,14 @@ const nextConfig = {
   images: {
     domains: ['tanal-app.onrender.com'],
   },
+  webpackDevMiddleware: config => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+      ignored: /node_modules/,
+    };
+    return config;
+  },
 };
+
 export default nextConfig;
