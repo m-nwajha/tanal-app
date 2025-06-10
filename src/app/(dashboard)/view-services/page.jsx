93182d695862405785b52e837ba87d9a111f56ff
Mohame ALnawjha <m.nawjha@gmail.com>
2025-06-10 15:52:10 +0300

    add ViewServices page.jsx

---
+++ src/app/(dashboard)/view-services/page.jsx
@@ -0,0 +1,8 @@
+import ViewServicesPage from '@/components/views/ViewServicesPage';
+import { PAGES_META_DATA } from '@/constants/META_DATA';
+export const metadata = {
+  title: PAGES_META_DATA.viewServices,
+};
+const ViewServices = () => <ViewServicesPage />;
+
+export default ViewServices;
