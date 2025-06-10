5d4e0448dc6c1d6b9ddca0c75cac5b7a24473daa
Mohame ALnawjha <m.nawjha@gmail.com>
2025-06-10 15:50:44 +0300

    add metadata

--- src/app/(dashboard)/create-service/page.jsx
+++ src/app/(dashboard)/create-service/page.jsx
@@ -1,5 +1,8 @@
 import CreateServicesPage from '@/components/views/CreateServicesPage';
-
+import { PAGES_META_DATA } from '@/constants/META_DATA';
+export const metadata = {
+  title: PAGES_META_DATA.createService,
+};
 const CreateService = () => <CreateServicesPage />
 
 export default CreateService
