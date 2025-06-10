fa42729db9894dac0da6b0227e0de6e7c2b1c79c
Mohame ALnawjha <m.nawjha@gmail.com>
2025-06-10 15:45:20 +0300

    add CookiesProvider

--- src/components/layouts/AppLayout.jsx
+++ src/components/layouts/AppLayout.jsx
@@ -1,19 +1,20 @@
 'use client';
-// Import Theme Provider .
+import React from 'react';
 import { ThemeProvider as MuiThemeProvider } from '@mui/material/styles';
 import { ThemeProvider as EmotionThemeProvider } from '@emotion/react';
-import { THEME } from '@/styles/theme';
-
-// Import Constants.
+import { CookiesProvider } from 'react-cookie';
 import { muiTheme } from '@/styles/createMuiTheme';
+import { THEME } from '@/styles/theme';
 import TopLoader from '../ui/TopLoader';
 
 const AppLayout = ({ children }) => {
   return (
     <MuiThemeProvider theme={muiTheme}>
       <EmotionThemeProvider theme={THEME}>
-        <TopLoader />
-        {children}
+        <CookiesProvider>
+          <TopLoader />
+          {children}
+        </CookiesProvider>
       </EmotionThemeProvider>
     </MuiThemeProvider>
   );
